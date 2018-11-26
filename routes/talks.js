6c9e6659f2e00bb85dba32846d3878c7e51e21eb
swoec <swoec.wang@gmail.com>
2018-11-26 16:59:47 +1300

    update

--- routes/talks.js
+++ routes/talks.js
@@ -3,29 +3,29 @@ var useragent = require('express-useragent');
 var db = monk('localhost:27017/lightningtalk');
 var topicss = db.get('topics');
 
-exports.list = function(req, res) {
+exports.list = function (req, res) {
     var holdtime = getDateTime();
-    topicss.find({"talktime":holdtime}).then((docs) => {
+    topicss.find({"talktime": holdtime}).then((docs) => {
         res.json(docs);
     }).then(() => db.close());
 };
 
-exports.findByName = function(req, res){
+exports.findByName = function (req, res) {
     var title = req.query.title;
-    topicss.find({"title":title}).then((topicsObj)=>{
-        res.json(topicsObj);
-    }).then(()=>db.close());
-};
-
-exports.getMax=function(req,res){
-    topicss.findOne({}, {sort: {id: -1}}).then((topicsObj)=>{
+    topicss.find({"title": title}).then((topicsObj) => {
         res.json(topicsObj);
     }).then(() => db.close());
 };
 
-exports.add = function(req, res) {
-    topicss.findOne({}, {sort: {id: -1}}).then((obj)=>{
-        var topics=req.body;
+exports.getMax = function (req, res) {
+    topicss.findOne({}, {sort: {id: -1}}).then((topicsObj) => {
+        res.json(topicsObj);
+    }).then(() => db.close());
+};
+
+exports.add = function (req, res) {
+    topicss.findOne({}, {sort: {id: -1}}).then((obj) => {
+        var topics = req.body;
         console.log(JSON.stringify(req.body));
         var source = req.headers['user-agent'];
         var ua = useragent.parse(source);
@@ -36,8 +36,8 @@ exports.add = function(req, res) {
         topics.ipaddr = req.headers['host'];
         topics.createtime = getDateTime();
         topics.talktime = getDateTime();
-        if(obj == null) topics.id = 1;
-        else topics.id=(parseInt(obj.id)+1)+"";
+        if (obj == null) topics.id = 1;
+        else topics.id = (parseInt(obj.id) + 1) + "";
 
         topicss.insert(topics).then((docs) => {
             res.json(docs);
@@ -50,7 +50,7 @@ function getDateTime() {
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     month = (month < 10 ? "0" : "") + month;
-    var day  = date.getDate();
+    var day = date.getDate();
     day = (day < 10 ? "0" : "") + day;
-    return year + "-" + month + "-" + day ;
+    return year + "-" + month + "-" + day;
 }
